e141ab8418e74f27c8df047dffbe291dc4e0967e
smcbrmmm <samut.c@ku.th>
2022-03-07 22:11:53 +0700

    ordeR

--- src/components/Order/Orders.jsx
+++ src/components/Order/Orders.jsx
@@ -14,7 +14,7 @@ export default function Orders() {
     useEffect(() => {
         const fetchData = async () => {
             const result = await axios(
-                'http://localhost:8090/api/order/order',
+                'https://7daf-2405-9800-b600-698c-5cad-e267-7f49-51f7.ngrok.io/api/order/order',
             );
 
             setOrder(result.data)
