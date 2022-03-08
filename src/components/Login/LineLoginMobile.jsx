db09baf558ca81144d20b626c401d8d1a06a12ca
smcbrmmm <samut.c@ku.th>
2022-03-08 13:17:45 +0700

    deploy

--- src/components/Login/LineLoginMobile.jsx
+++ src/components/Login/LineLoginMobile.jsx
@@ -59,7 +59,8 @@ export default function LineLoginMobile() {
 
                     setName(liff.getProfile.displayName)
                     setAccessToken(liff.getAccessToken)
-                    setEmail(liff.getDecodedIDToken().email)
+                    
+                    console.log(isHave(liff.getDecodedIDToken().email))
 
                 } else {
                     liff.login();
