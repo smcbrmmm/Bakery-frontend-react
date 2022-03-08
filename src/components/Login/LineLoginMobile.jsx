1358546082b65094eb5bc45915bff3e5e38df1d5
smcbrmmm <samut.c@ku.th>
2022-03-08 14:48:42 +0700

    deploy

--- src/components/Login/LineLoginMobile.jsx
+++ src/components/Login/LineLoginMobile.jsx
@@ -188,6 +188,7 @@ export default function LineLoginMobile() {
 
             <SigninModal show={signinModalShow}
                 onHide={() => setSigntinModalShow(false)}
+                email={email}
             />
 
         </div>
