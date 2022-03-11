b62e891ee1306425fb6997408bced89fa1be98c7
smcbrmmm <samut.c@ku.th>
2022-03-12 03:27:06 +0700

    deploy

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -188,21 +188,23 @@ const Products = ({ products, setProductList, setProductInCart }) => {
                     <h1 className="main" style={{ textAlign: 'center' }}>  Mymom Bakery's Menu </h1>
                 </Container> */}
 
-                <Container style={{ marginTop: '4rem' }}>
+                <Container >
                     <Row style={{ marginBottom: '1rem' }}>
                         <Col sm={3}>
 
-
-                            <MediaQuery minWidth={1224}>
+                            <MediaQuery minWidth={1224} style={{marginTop:'4rem'}}>
                                 <h2 className="main" style={{ marginBottom: '2rem' }}>  Category </h2>
+                                <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("pastry")} >  Chinese Pastry </h5></a>
+                                <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("roastedPastry")}>  Roasted Chinese Pastry </h5></a>
+                                <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("riceCracker")}>  Rice Cracker </h5></a>
                             </MediaQuery>
                             <MediaQuery maxWidth={1224}>
-                                <h2 className="main" >  Category </h2>
+                                <h2 className="main" style={{ marginBottom: '2rem' }}>  Category </h2>
+                                <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("pastry")} >  Chinese Pastry </h5></a>
+                                <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("roastedPastry")}>  Roasted Chinese Pastry </h5></a>
+                                <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("riceCracker")}>  Rice Cracker </h5></a>
+                                <hr></hr>
                             </MediaQuery>
-
-                            <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("pastry")} >  Chinese Pastry </h5></a>
-                            <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("roastedPastry")}>  Roasted Chinese Pastry </h5></a>
-                            <a href="#" className="cat-menu"><h5 className="cat-menu" onClick={() => categoryFilter("riceCracker")}>  Rice Cracker </h5></a>
                         </Col>
                         <Col sm={9}>
                             <h2 className="main" style={{ textAlign: 'left', marginBottom: '2rem' }} >  Products
