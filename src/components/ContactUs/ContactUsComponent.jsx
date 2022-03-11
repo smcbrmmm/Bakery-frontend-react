8dbd379ecf44429e7442d41f1a4e1891ed1aec1b
smcbrmmm <samut.c@ku.th>
2022-03-12 02:01:32 +0700

    deploy

--- src/components/ContactUs/ContactUsComponent.jsx
+++ src/components/ContactUs/ContactUsComponent.jsx
@@ -6,32 +6,52 @@ import GoogleMapReact from 'google-map-react';
 
 // Redux
 import { connect } from "react-redux";
+import MediaQuery from 'react-responsive'
 
-const AnyReactComponent = ({ text }) => <div>{text} <img src="https://img.icons8.com/external-kmg-design-flat-kmg-design/32/000000/external-pin-maps-navigation-kmg-design-flat-kmg-design-1.png"></img></div>;
+const AnyReactComponent = ({ text }) => <div>{text}
+    <img src="https://img.icons8.com/external-kmg-design-flat-kmg-design/32/000000/external-pin-maps-navigation-kmg-design-flat-kmg-design-1.png">
+    </img></div>;
 
 
 const ContactUsComponent = () => {
 
     return (
         <div className="page-container">
-            
+
             <NavbarCom />
             <div className="content-wrap">
                 <h1 className="main" style={{ textAlign: 'center' }}>  Contact us </h1>
-                <Container style={{ height: '500px', width: '600px' }}>
-                    <GoogleMapReact
-                        bootstrapURLKeys={{ key: 'AIzaSyBnEaWzZ7b4BVzDtSiIuJLx2iK6-CH75Yw', language: 'en' }}
-                        defaultCenter={{ lat: 13.816659689024158, lng: 100.05297262248749 }}
-                        defaultZoom={17}
-                    >
-                        <AnyReactComponent
-                            lat={13.8172596890244}
-                            lng={100.0527726224849}
-                            text="MyMom Bakery"
 
-                        />
-                    </GoogleMapReact>
-                </Container>
+                <MediaQuery minWidth={1224}>
+                    <Container style={{ height: '500px', width: '600px' }}>
+                        <GoogleMapReact
+                            bootstrapURLKeys={{ key: 'AIzaSyBnEaWzZ7b4BVzDtSiIuJLx2iK6-CH75Yw', language: 'en' }}
+                            defaultCenter={{ lat: 13.816659689024158, lng: 100.05297262248749 }}
+                            defaultZoom={17}>
+                            <AnyReactComponent
+                                lat={13.8172596890244}
+                                lng={100.0527726224849}
+                                text="MyMom Bakery"
+                            />
+                        </GoogleMapReact>
+                    </Container>
+                </MediaQuery>
+                <MediaQuery maxWidth={1224}>
+                    <Container style={{ height: '200px', width: '300px' }}>
+                        <GoogleMapReact
+                            bootstrapURLKeys={{ key: 'AIzaSyBnEaWzZ7b4BVzDtSiIuJLx2iK6-CH75Yw', language: 'en' }}
+                            defaultCenter={{ lat: 13.816659689024158, lng: 100.05297262248749 }}
+                            defaultZoom={17}>
+                            <AnyReactComponent
+                                lat={13.8172596890244}
+                                lng={100.0527726224849}
+                                text="MyMom Bakery"
+                            />
+                        </GoogleMapReact>
+                    </Container>
+                </MediaQuery>
+
+
             </div>
 
         </div>
